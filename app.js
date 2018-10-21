af9dc5d3f52f72b2e79b9229c5ad0b0f5480015a
Harsh Patel <pateltharsh@gmail.com>
2018-10-21 20:46:23 +0000

    Refactored app.js

--- app.js
+++ app.js
@@ -1,31 +1,17 @@
 var express     = require("express"),
     app         = express(),
     bodyParser  = require("body-parser"),
-    mongoose    = require("mongoose")
+    mongoose    = require("mongoose"),
+    Campground  = require("./models/campground")
     
 mongoose.connect("mongodb://localhost/yelp_camp", { useNewUrlParser: true });
 app.use(bodyParser.urlencoded({extended: true}));
 app.set("view engine", "ejs");
 
-// SCHEMA Setup
-var campgroundSchema = new mongoose.Schema({
-    name: String,
-    image: String,
-    description: String
-});
-var Campground = mongoose.model("Campground", campgroundSchema);
-
 // INDEX route - show all campgrounds
 app.get("/", function(req, res) {
     res.render("landing");
 });
-
-// var campgrounds = [
-//             {name: "Salmon Creek", image:"https://pixabay.com/get/eb3db30a29fd063ed1584d05fb1d4e97e07ee3d21cac104491f0c178a4ecb6be_340.jpg"},
-//             {name: "Granite Hill", image:"https://pixabay.com/get/e833b3092cf5033ed1584d05fb1d4e97e07ee3d21cac104491f0c178a4ecb6be_340.jpg"},
-//             {name: "Moiuntain Goat's Rest", image:"https://pixabay.com/get/e834b70c2cf5083ed1584d05fb1d4e97e07ee3d21cac104491f0c178a4ecb6be_340.jpg"}
-//     ];
-
 
 // CREATE route - add new campground to the database
 app.get("/campgrounds", function(req, res) {
