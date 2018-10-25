00152b7cad3821775427176faaeb188ee99f48e6
Harsh Patel <pateltharsh@gmail.com>
2018-10-25 21:28:01 +0000

    Added error handling with flash

--- routes/comments.js
+++ routes/comments.js
@@ -26,6 +26,7 @@ router.post("/campgrounds/:id/comments", middleware.isLoggedIn, function(req, re
             // create new comment
             Comment.create(req.body.comment, function(err, comment) {
                 if (err) {
+                    req.flash("error", "Something went wrong");
                     console.log(err);
                 } else {
                     comment.author.id = req.user._id;
@@ -33,6 +34,7 @@ router.post("/campgrounds/:id/comments", middleware.isLoggedIn, function(req, re
                     comment.save();
                     campground.comments.push(comment);
                     campground.save();
+                    req.flash("success", "Successfully added comment");
                     res.redirect("/campgrounds/" + campground._id);
                 }
             });
@@ -68,6 +70,7 @@ router.delete("/campgrounds/:id/comments/:comment_id", middleware.checkCommentOw
        if(err) {
            res.redirect("back");
        } else {
+           req.flash("success", "Comment deleted");
            res.redirect("/campgrounds/" + req.params.id);
        }
    });
