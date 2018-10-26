952a7422957defebf0cb116a1165e13eb0d5a58d
Harsh Patel <pateltharsh@gmail.com>
2018-10-26 23:29:07 +0000

    Added error handling

--- routes/campgrounds.js
+++ routes/campgrounds.js
@@ -2,6 +2,12 @@ var express = require("express");
 var router = express.Router();
 var Campground = require("../models/campground");
 var middleware = require("../middleware");
+var geocoder = require('geocoder');
+var { isLoggedIn, checkCampgroundOwnership, checkCommentOwnership, isAdmin, isSafe } = middleware;
+
+function escapeRegex(text) {
+    return text.replace(/[-[\]{}()*+?.,\\^$|#\s]/g, "\\$&");
+};
 
 router.get("/campgrounds", function(req, res) {
     Campground.find({}, function(err, allCampgrounds){
@@ -24,15 +30,25 @@ router.post("/campgrounds", middleware.isLoggedIn, function(req, res) {
         id: req.user._id,
         username: req.user.username
     };
-    var newCampground = {name: name, price: price, image: image, description: desc, author: author};
-    // Create a new campground and save to DB
-    Campground.create(newCampground, function(err, newlyCreated){
-        if (err) {
-            console.log(err);
-        } else {
-            // Redirect back to campgrounds page
-            res.redirect("/campgrounds", {campgrounds: newlyCreated, currentUser: req.user});
+    var cost = req.body.cost;
+    geocoder.geocode(req.body.location, function (err, data) {
+        if (err || data.status === 'ZERO_RESULTS') {
+            req.flash('error', 'Invalid address');
+            return res.redirect('back');
         }
+        var lat = data.results[0].geometry.location.lat;
+        var lng = data.results[0].geometry.location.lng;
+        var location = data.results[0].formatted_address;
+        var newCampground = {name: name, price: price, image: image, description: desc, author: author, location: location, lat: lat, lng: lng};
+    // Create a new campground and save to DB
+        Campground.create(newCampground, function(err, newlyCreated){
+            if (err) {
+                console.log(err);
+            } else {
+            // Redirect back to campgrounds page
+                res.redirect("/campgrounds", {campgrounds: newlyCreated, currentUser: req.user});
+            }
+        });
     });
 });
 
